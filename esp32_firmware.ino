bc14cc72d21010c67a86a9b96a9fc4a342b9c8b2
Suraj Mandal <surajmandalcell@gmail.com>
2024-06-23 06:44:20 -0400

    Update server.cpp and server.h for handling HTTP requests and refactor code to separate server functionality

--- esp32_firmware.ino
+++ esp32_firmware.ino
@@ -1,6 +1,6 @@
-#include "services/config.h"
 #include "server.h"
 #include "services/bluetooth_manager.h"
+#include "services/config.h"
 
 void setup() {
   Serial.begin(115200);
--- server.cpp
+++ server.cpp
@@ -1,13 +1,13 @@
-#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
 
 #include "services/bluetooth_manager.h"
 #include "services/config.h"
 #include "services/wifi_manager.h"
 
-WebServer server(80);
+AsyncWebServer server(80);
 
-void handleRoot() {
-  server.send(200, "text/plain", "Device Info: ESP32");
+void handleRoot(AsyncWebServerRequest *request) {
+  request->send(200, "text/plain", "Device Info: ESP32");
 }
 
 void setupServer() {
@@ -15,35 +15,53 @@ void setupServer() {
 
   // Bluetooth Endpoints
 
-  server.on("/v1/bluetooth/enable", HTTP_GET, []() {
-    enableBluetooth();
-    server.send(200, "text/plain", "Bluetooth enabled");
-  });
-  server.on("/v1/bluetooth/disable", HTTP_GET, []() {
-    disableBluetooth();
-    server.send(200, "text/plain", "Bluetooth disabled");
-  });
+  server.on(
+      "/v1/bluetooth/enable", HTTP_GET, [](AsyncWebServerRequest *request) {
+        enableBluetooth();
+        request->send(200, "text/plain", "Bluetooth enabled");
+      });
+  server.on(
+      "/v1/bluetooth/disable", HTTP_GET, [](AsyncWebServerRequest *request) {
+        disableBluetooth();
+        request->send(200, "text/plain", "Bluetooth disabled");
+      });
 
   // WIFI Endpoints
 
-  server.on("/v1/wifi/connect", HTTP_POST, []() {
-    connectWiFi(ssid.c_str(), password.c_str());
-    server.send(200, "text/plain", "Connecting to WiFi...");
-  });
+  server.on(
+      "/v1/wifi/connect", HTTP_POST, [](AsyncWebServerRequest *request) {},
+      [](AsyncWebServerRequest *request, uint8_t *data, size_t len,
+          size_t index, size_t total) {
+        // Variables to store SSID and password
+        String ssid = "";
+        String password = "";
 
-  server.on("/v1/wifi/disconnect", HTTP_GET, []() {
-    disconnectWiFi();
-    server.send(200, "text/plain", "Disconnected from WiFi");
-  });
+        // Parse POST data
+        if (request->hasParam("ssid", true) &&
+            request->hasParam("password", true)) {
+          ssid = request->getParam("ssid", true)->value();
+          password = request->getParam("password", true)->value();
+          connectWiFi(ssid.c_str(), password.c_str());
+          request->send(200, "text/plain", "Connecting to WiFi...");
+        } else {
+          request->send(400, "text/plain", "Missing SSID or password");
+        }
+      });
 
-  server.on("/v1/wifi/scan", HTTP_GET, []() {
-    scanNetworks();
-    server.send(200, "text/plain", "Scanning WiFi networks...");
+  server.on(
+      "/v1/wifi/disconnect", HTTP_GET, [](AsyncWebServerRequest *request) {
+        disconnectWiFi();
+        request->send(200, "text/plain", "Disconnected from WiFi");
+      });
+
+  server.on("/v1/wifi/scan", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String networks =
+        scanNetworks();  // assuming this function returns a string
+    request->send(200, "text/plain", networks);
   });
 
   server.begin();
 }
 
 void loopServer() {
-  server.handleClient();
 }
